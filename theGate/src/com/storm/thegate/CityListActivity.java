580ca605d62b45efb94ed826fb616dbe30946769
wushujia <wu.shujia@pivosgroup.com.cn>
2016-12-08 17:03:17 +0800

    first commit

---
+++ theGate/src/com/storm/thegate/CityListActivity.java
@@ -0,0 +1,13 @@
+package com.storm.thegate;
+
+import android.os.Bundle;
+
+import com.storm.thegate.base.BaseActivity;
+
+public class CityListActivity extends BaseActivity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
